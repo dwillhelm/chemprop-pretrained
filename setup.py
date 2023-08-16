dc45c09bfd6dc34008f7c0b855a09a97a378615f
dwillhelm <dwillhelm@enthought.com>
2023-08-16 18:53:05 +0200

    init setup py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "chemprop_pretrained"
+PACKAGE_DIRS = ["src"]
+VERSION = "0.0.0"
+RUNTIME_DEPS = None
+
+
+def main():
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        install_requires=RUNTIME_DEPS,
+        packages=find_packages(include=PACKAGE_DIRS),
+    )
+
+if __name__ == "__main__":
+    main()
