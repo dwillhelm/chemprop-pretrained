6c52f72ee6ef0184ef9773205d1420ff5cf1978b
Daniel <45088478+dwillhelm@users.noreply.github.com>
2023-10-11 13:33:41 -0500

    ref/cln; update docstring (#6)

--- chemprop_pretrained/predict.py
+++ chemprop_pretrained/predict.py
@@ -1,26 +1,40 @@
+from pathlib import Path
 from typing import Union, List, Any 
 from os import PathLike
 
 import chemprop
 
 class ChempropPredict: 
+    """Chemprop inference class."""
 
-    def __init__(self, checkpoint_path:Union[str, PathLike]): 
-        self.checkpoint_path = checkpoint_path
+    def __init__(self, checkpoint_path:Union[str, PathLike]):
+        """Initialize.  
+
+        Parameters
+        ----------
+        checkpoint_path : Union[str, PathLike]
+            Path to a persisted Chemprop model/checkpoint (.pt) file. 
+        """
+        self.checkpoint_path = Path(checkpoint_path)
 
     def predict(self, smiles:List[str]): 
+        """Forward prediction. 
 
+        Parameters
+        ----------
+        smiles : List[str]
+            A list of SMILES strings
+        """
         arguments = [
             '--test_path', '/dev/null',
             '--preds_path', '/dev/null',
-            '--checkpoint_path', '../models/model-bs.pt'
+            '--checkpoint_path', str(self.checkpoint_path)
         ]
-
         args = chemprop.args.PredictArgs().parse_args(arguments)
-        model_objects = chemprop.train.load_model(args=args)
 
-        preds = chemprop.train.make_predictions(
+        model_objects = chemprop.train.load_model(args=args)
+        y_pred = chemprop.train.make_predictions(
             args=args,
             smiles=smiles, 
             model_objects=model_objects)
-        return preds
+        return y_pred
